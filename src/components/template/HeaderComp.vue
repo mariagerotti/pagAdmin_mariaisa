b93727510e90d10b77173c01afd43608cf09820c
mariagerotti <mariaeduarda.gerotti@gmail.com>
2022-06-23 15:11:00 -0300

    upload

---
+++ src/components/template/HeaderComp.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <div class="menu-header">
+      <span> <RouterLink to="/">HOME</RouterLink> </span>
+
+      <span> <RouterLink to="/livros">LIVROS</RouterLink> </span>
+
+      <span> <RouterLink to="/editoras">EDITORAS</RouterLink> </span>
+
+      <span> <RouterLink to="/autores">AUTORES</RouterLink> </span>
+
+      <span> <RouterLink to="/categorias">CATEGORIAS</RouterLink> </span>
+    </div>
+  </header>
+
+  <div class="search-bar">
+    <nav class="navbar navbar-light bg-light">
+      <form class="form-inline">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Adicionar"
+          aria-label="Search"
+        />
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          +
+        </button>
+      </form>
+    </nav>
+  </div>
+</template>
+
+<style>
+header span {
+  padding-right: 2%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/editoras',
-      name: 'editoras',
-      component: () => import('../views/EditorasView.vue')
-    }
-  ]
-})
+      path: "/editoras",
+      name: "editoras",
+      component: () => import("../views/EditorasView.vue"),
+    },
+    {
+      path: "/livros",
+      name: "livros",
+      component: () => import("../views/LivrosView.vue"),
+    },
+    {
+      path: "/autores",
+      name: "autores",
+      component: () => import("../views/AutoresView.vue"),
+    },
+    {
+      path: "/categorias",
+      name: "categorias",
+      component: () => import("../views/CategoriasView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;

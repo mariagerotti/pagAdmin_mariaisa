28133b3935df5cb968bc031100c2f031bfde9ac2
mariagerotti <mariaeduarda.gerotti@gmail.com>
2022-06-24 08:59:06 -0300

    upload

--- src/components/template/HeaderComp.vue
+++ src/components/template/HeaderComp.vue
@@ -1,6 +1,20 @@
 <template>
   <header>
     <div class="menu-header">
+      <!-- Image and text 
+      <nav class="navbar navbar-light">
+        <a class="navbar-brand" href="#">
+          <img
+            src="/docs/4.2/assets/brand/bootstrap-solid.svg"
+            width="30"
+            height="30"
+            class="d-inline-block align-top"
+            alt=""
+          />
+          Bootstrap
+        </a>
+      </nav>-->
+
       <span> <RouterLink to="/">HOME</RouterLink> </span>
 
       <span> <RouterLink to="/livros">LIVROS</RouterLink> </span>
@@ -14,7 +28,7 @@
   </header>
 
   <div class="search-bar">
-    <nav class="navbar navbar-light bg-light">
+    <nav class="navbar navbar-light">
       <form class="form-inline">
         <input
           class="form-control mr-sm-2"
@@ -34,4 +48,21 @@
 header span {
   padding-right: 2%;
 }
+
+header {
+  border-bottom: 1px solid black;
+  padding: 1%;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+.search-bar {
+  /*float: right;*/
+  padding-right: 1%;
+  margin-top: 1%;
+}
+
 </style>
